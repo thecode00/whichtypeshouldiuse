c138e8a16f2ac9e6d7716270846ba86f4e4c7184
thecode00 <thecode0087@gmail.com>
2023-10-27 05:35:10 +0900

    Add style

--- src/components/UI/VariableBlock.js
+++ src/components/UI/VariableBlock.js
@@ -1,47 +1,56 @@
 import React, { useContext, useState } from "react";
 import LanguageContext from "../../context/language-context";
+import classes from "./VariableBlock.module.css";
 
 function VariableBlock() {
-	const languageCtx = useContext(LanguageContext);
-	const [name, setName] = useState("");
-	const [maxNum, setMaxNum] = useState(0);
-	const [minNum, setMinNum] = useState(0);
-	const [type, setType] = useState("");
+  const languageCtx = useContext(LanguageContext);
+  const [name, setName] = useState("");
+  const [maxNum, setMaxNum] = useState(0);
+  const [minNum, setMinNum] = useState(0);
+  const [type, setType] = useState("");
 
-	const nameChangeHandler = (event) => {
-		setName(event.target.value);
-	};
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
 
-	const minNumberChangeHandler = (event) => {
-		setMinNum(event.target.value);
-		setType(languageCtx.onCheckType(event.target.value, maxNum));
-	};
+  const minNumberChangeHandler = (event) => {
+    setMinNum(event.target.value);
+    setType(languageCtx.onCheckType(event.target.value, maxNum));
+  };
 
-	const maxNumberChangeHandler = (event) => {
-		setMaxNum(event.target.value);
-		setType(languageCtx.onCheckType(minNum, event.target.value));
-	};
-	return (
-		<>
-			<label htmlFor="name" />
-			<input id="name" type="text" onBlur={nameChangeHandler} />
-			<label htmlFor="minNum" />
-			<input
-				id="minNum"
-				type="number"
-				value={minNum}
-				onChange={minNumberChangeHandler}
-			/>
-			<label htmlFor="maxNum" />
-			<input
-				id="maxNum"
-				type="number"
-				value={maxNum}
-				onChange={maxNumberChangeHandler}
-			/>
-			<p>{type}</p>
-		</>
-	);
+  const maxNumberChangeHandler = (event) => {
+    setMaxNum(event.target.value);
+    setType(languageCtx.onCheckType(minNum, event.target.value));
+  };
+  return (
+    <div className={classes.block}>
+      <div className={classes["display-block"]}>
+        <label htmlFor="name">Variable name</label>
+        <input id="name" type="text" onBlur={nameChangeHandler} />
+      </div>
+      <div className={classes["display-block"]}>
+        <label htmlFor="minNum">Min number</label>
+        <input
+          id="minNum"
+          type="number"
+          value={minNum}
+          onChange={minNumberChangeHandler}
+        />
+      </div>
+      <div className={classes["display-block"]}>
+        <label htmlFor="maxNum">Max number</label>
+        <input
+          id="maxNum"
+          type="number"
+          value={maxNum}
+          onChange={maxNumberChangeHandler}
+        />
+      </div>
+      <div className={classes["display-block"]}>
+        <p>{type}</p>
+      </div>
+    </div>
+  );
 }
 
 export default VariableBlock;
